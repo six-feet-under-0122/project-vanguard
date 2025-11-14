03dfd1c30f3ab294a43e58c8fabadea58819acb9
six-feet-under-virtual-box <qjl070806@outlook.com>
2025-11-14 12:24:21 +0800

    feat：add SingleDogNode class (oop)

---
+++ task5/oop.cpp
@@ -0,0 +1,21 @@
+#include "rclcpp/rclcpp.hpp"
+
+class SingleDogNode : public rclcpp::Node
+{
+    public:
+    SingleDogNode(std::string name) : Node(name)
+    {
+        RCLCPP_INFO(this->get_logger(),"helloworldd %s",name.c_str());
+    }
+    private:
+};
+int main (int argc,char **argv)
+{
+    using namespace std;
+    using namespace rclcpp;
+    init(argc,argv);
+    auto node =make_shared<SingleDogNode>("evan");
+    spin(node);
+    shutdown();
+    return 0;
+}
